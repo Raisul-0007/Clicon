acaa6295dc67f3b43422e717649b65d35aee1025
Raisul-0007 <raisulislamriat007@gmail.com>
2026-01-03 21:00:04 +0600

    navtwo start

--- app/components/NavberTwo.jsx
+++ app/components/NavberTwo.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Container from './Container';
+import { IoIosArrowDown } from 'react-icons/io';
 
 const NavberTwo = () => {
   return (
     <div>
-      <Container></Container>
+      <Container>
+        <div className="">
+          <ul>
+            <li className='font-publicSans text-md'>All Category <IoIosArrowDown /></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className=""></div>
+      </Container>
     </div>
   )
 }
