90cf52d906229efd7beedc7af5ea0b913b41d1bd
Raisul-0007 <raisulislamriat007@gmail.com>
2026-01-05 18:36:52 +0600

    ban complete

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -11,9 +11,39 @@ const Banner = () => {
             <Image src="/BanOne.png" alt="Banner" width={1200} height={400} />
             </div>
             <div className="w-2/6">
-                <Image className='pb-4' src="/BanTwo.png" alt="Banner" width={400} height={400} />
-                <Image className='pt-4' src="/BanThree.png" alt="Banner" width={400} height={400} />
+                <Image className='pb-2' src="/BanTwo.png" alt="Banner" width={500} height={350} />
+                <Image className='pt-2' src="/BanThree.png" alt="Banner" width={500} height={350} />
             </div>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <Image src="/banIconOne.png" alt="one" height={40} width={40}/>
+            <div className="">
+              <h4>Fasted Delivery</h4>
+              <p className='text-[#5F6C72] text-[12px]'>Delivery in 24/H</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <Image src="/banIconTwo.png" alt="Two" height={40} width={40}/>
+            <div className="">
+              <h4>24 Hours Return</h4>
+              <p className='text-[#5F6C72] text-[12px]'>100% money-back guarantee</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <Image src="/banIconThree.png" alt="Three" height={40} width={40}/>
+            <div className="">
+              <h4>Secure Payment</h4>
+              <p className='text-[#5F6C72] text-[12px]'>Your money is safe</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <Image src="/banIconFour.png" alt="Four" height={40} width={40}/>
+            <div className="">
+              <h4>Support 24/7</h4>
+              <p className='text-[#5F6C72] text-[12px]'>Live contact/message</p>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Products from './components/Products';
 import Banner from './components/Banner';
+import Category from './components/Category';
 export default function Home() {
   return (
     <div className="">
       <Banner/>
       <Products/>
+      <Category/>
     </div>
   );
 }
