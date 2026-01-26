c5daa4e9dbdb1be2418c81579c13a20fa43a6751
Raisul-0007 <raisulislamriat007@gmail.com>
2026-01-26 23:03:58 +0600

    product details title add

--- app/(root)/shop/[id]/page.jsx
+++ app/(root)/shop/[id]/page.jsx
@@ -39,8 +39,33 @@ const ProductDetails = () => {
                 {product.rating}
               </div>
             </div>
+            <div className="py-3">
+              <h2 className='font-publicSans text-3xl'>{product.title}</h2>
+            </div>
             <div className="">
-              {product.title}
+              <div className="flex justify-between py-1">
+                <div className="flex gap-2">
+                  <p className='font-publicSans text-gray-600'>Sku:</p>
+                  <h6 className='font-publicSans'>{product.sku}</h6>
+                </div>
+                <div className="flex gap-2">
+                  <p className='font-publicSans text-gray-600'>Availability:</p>
+                  <h6 className='font-publicSans text-[#2DB224]'>{product.availabilityStatus}</h6>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="flex gap-2">
+                  <p className='font-publicSans text-gray-600'>Brand:</p>
+                  <h6 className='font-publicSans'>{product.brand}</h6>
+                </div>
+                <div className="flex gap-2">
+                  <p className='font-publicSans text-gray-600'>Category:</p>
+                  <h6 className='font-publicSans'>{product.category}</h6>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              {product.size}
             </div>
           </div>
         </div>
