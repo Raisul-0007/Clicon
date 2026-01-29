fdfaff44715f385f136845e17c51839d8a644cca
Raisul-0007 <raisulislamriat007@gmail.com>
2026-01-29 23:14:40 +0600

    Product Details Complete

--- app/(root)/shop/[id]/page.jsx
+++ app/(root)/shop/[id]/page.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 import  Image  from 'next/image';
 import Loading from './../../../loading';
 import Rating from '../../../components/Rating';
+import HomeProduct from './../../../components/HomeProduct';
 
 const ProductDetails = () => {
     const Product  = useParams()
@@ -16,6 +17,9 @@ const ProductDetails = () => {
     .then(data => setProduct(data))
     .catch(err => console.log(err))
   }
+  let handleCart =(item)=>{
+    dispatchEvent(addToCart({...item, qun:1}))
+  }
 
   useEffect(() => {
     singleProduct()
@@ -23,14 +27,17 @@ const ProductDetails = () => {
   if(!product){
     return <Loading/>
   }
+  let discountPrice = (product.price / 100) * product.discountPercentage;
+    let mainPrice = product.price - discountPrice
   return (
     <div>
       <Container>
         <div className="flex">
-          <div className="w-1/2 p-5">
-          <Image className=' border border-gray-300' src={product.thumbnail} alt='img'  height={200} width={600} />
+          <div className="w-1/2 relative h-120 p-5">
+          <Image className=' border border-gray-300 px-10' src={product.thumbnail} alt='img'  fill />
           </div>
           <div className="w-1/2 p-5">
+
             <div className=" flex items-center gap-3">
               <div className="flex gap-1  text-[#FA8232]">
               <Rating rating={product.rating}/>
@@ -39,9 +46,10 @@ const ProductDetails = () => {
                 {product.rating}
               </div>
             </div>
-            <div className="py-3">
+            <div className="py-2">
               <h2 className='font-publicSans text-3xl'>{product.title}</h2>
             </div>
+            <div className="py-6">
             <div className="">
               <div className="flex justify-between py-1">
                 <div className="flex gap-2">
@@ -67,7 +75,29 @@ const ProductDetails = () => {
             <div className="">
               {product.size}
             </div>
+            </div>
+            <div className="flex gap-2 items-center">
+          <div className="">
+           <h4 className='font-publicSans text-2xl text-[#2DA5F3]'>{mainPrice.toFixed(2)}</h4> 
           </div>
+          <div className="font-publicSans text-[18px] text-[#77878F]">
+            <del>{product.price}</del>
+          </div>
+          <div className="bg-[#EFD33D] hover:bg-white hover:text-black py-2 px-4 rounded">
+            <h5 className='font-publicSans text-sm'>{product.discountPercentage}% OFF</h5>
+          </div>
+        </div>
+        <div className="py-10 ">
+          <h5 onClick={()=>handleCart(product)} className='font-publicSans bg-[#FA8232] py-5 text-white text-center rounded-xl'>Add to Cart</h5>
+        </div>
+        <div className=" py-6 border-2 border-gray-200 px-4 shadow-x">
+          <p className='font-publicSans text-sm pb-3 '>100% Guarantee Safe Checkout</p>
+          <Image src="/PaymentMethod.png" alt="payment" width={300} height={100}/>
+        </div>
+          </div>
+        </div>
+        <div className="py-20">
+          <HomeProduct/>
         </div>
       </Container>
     </div>
