1fb9ebec510c2c7597548ae823033bc7d5756f17
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-03 11:08:42 +0600

    header responsive

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,17 +6,17 @@ import Container from './Container';
 const Header = () => {
   return (
     <div className='bg-black '>
-      <Container className="flex items-center justify-between py-1.5 px-4">
-        <div className=" ">
-        <Link className="" href="/"><Image src="/header.png" height="45" width="160" alt="header"/></Link>
+      <Container className="flex items-center justify-between md:py-1.5 py-2 px-4">
+        <div className="">
+          <Image className='md:w-40 w-20' src="/header.png" height="40" width="160" alt="header"/>
       </div>
       <div className="flex gap-3 items-center">
         <p className="font-publicSans text-sm text-white">up to</p>
-        <h3 className="font-publicSans text-[40px] text-[#EBC80C]">59%</h3>
-        <h4 className="font-publicSans text-md text-white">OFF</h4>
+        <h3 className="font-publicSans md:text-[40px] text-[#EBC80C] text-[12px]">59%</h3>
+        <h4 className="font-publicSans text-md text-white md:text-[17px] text-[12px]">OFF</h4>
       </div>
       <div className="">
-       <Link href="/shop" className='bg-[#EBC80C] uppercase flex gap-2 items-center px-4 py-3'>Shop Now <BsArrowRight/></Link> 
+       <Link href="/shop" className='bg-[#EBC80C] uppercase flex items-center gap-3 md:px-4 px-2 md:py-3 py-1 md:text-[16px] text-[12px]'>Shop Now <BsArrowRight/></Link> 
       </div>
       </Container>
     </div>
