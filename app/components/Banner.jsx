94e55f6d90dcc34d98ba1b202bbd57a6d6c002cf
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-04 21:12:04 +0600

    banner responsive

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -15,33 +15,33 @@ const Banner = () => {
                 <Image className='pt-2' src="/BanThree.png" alt="Banner" width={500} height={350} />
             </div>
         </div>
-        <div className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <Image src="/banIconOne.png" alt="one" height={40} width={40}/>
+        <div className="flex flex-wrap justify-between">
+          <div className="md:w-1/4 w-2/4 py-2 flex items-center gap-2">
+            <Image className='md:w-10 w-8' src="/banIconOne.png" alt="one" height={40} width={40}/>
             <div className="">
-              <h4 className='font-publicSans'>Fasted Delivery</h4>
-              <p className='font-publicSans text-[#5F6C72] text-[12px]'>Delivery in 24/H</p>
+              <h4 className='font-publicSans md:text-[16px] text-[14px]'>Fasted Delivery</h4>
+              <p className='font-publicSans text-[#5F6C72] md:text-[12px] text-[10px]'>Delivery in 24/H</p>
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <Image src="/banIconTwo.png" alt="Two" height={40} width={40}/>
+          <div className="md:w-1/4 w-2/4 py-2 flex items-center gap-2">
+            <Image className='md:w-10 w-8' src="/banIconTwo.png" alt="Two" height={40} width={40}/>
             <div className="">
-              <h4 className='font-publicSans'>24 Hours Return</h4>
-              <p className='font-publicSans text-[#5F6C72] text-[12px]'>100% money-back guarantee</p>
+              <h4 className='font-publicSans md:text-[16px] text-[14px]'>24 Hours Return</h4>
+              <p className='font-publicSans text-[#5F6C72] md:text-[12px] text-[10px]'>100% money-back guarantee</p>
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <Image src="/banIconThree.png" alt="Three" height={40} width={40}/>
+          <div className="md:w-1/4 w-2/4 py-2 flex items-center gap-2">
+            <Image className='md:w-10 w-8' src="/banIconThree.png" alt="Three" height={40} width={40}/>
             <div className="">
-              <h4 className='font-publicSans'>Secure Payment</h4>
-              <p className='font-publicSans text-[#5F6C72] text-[12px]'>Your money is safe</p>
+              <h4 className='font-publicSans md:text-[16px] text-[14px]'>Secure Payment</h4>
+              <p className='font-publicSans text-[#5F6C72] md:text-[12px] text-[10px]'>Your money is safe</p>
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <Image src="/banIconFour.png" alt="Four" height={40} width={40}/>
+          <div className="md:w-1/4 w-2/4 py-2 flex items-center gap-2">
+            <Image className='md:w-10 w-8' src="/banIconFour.png" alt="Four" height={40} width={40}/>
             <div className="">
-              <h4 className='font-publicSans'>Support 24/7</h4>
-              <p className='font-publicSans text-[#5F6C72] text-[12px]'>Live contact/message</p>
+              <h4 className='font-publicSans md:text-[16px] text-[14px]'>Support 24/7</h4>
+              <p className='font-publicSans text-[#5F6C72] md:text-[12px] text-[10px]'>Live contact/message</p>
             </div>
           </div>
         </div>
