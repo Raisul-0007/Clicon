fb096369f314f33abd0b52ddd178dc8bd0ab3470
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-05 13:42:44 +0600

    responive newsLetter

--- app/components/NewsLetter.jsx
+++ app/components/NewsLetter.jsx
@@ -7,19 +7,19 @@ import Image from 'next/image';
 const NewsLetter = () => {
   return (
     <div className='bg-[#1B6392]'>
-      <Container className="w-150 py-15">
-        <h3 className='font-publicSans text-[32px] font-semibold text-white text-center'>Subscribe to our newsletter</h3>
-        <p className='font-publicSans text-sm text-gray-300 text-center py-5 px-15'>Praesent fringilla erat a lacinia egestas. Donec vehicula tempor libero et cursus. Donec non quam urna. Quisque vitae porta ipsum.</p>
+      <Container className="lg:w-1/3 py-15">
+        <h3 className='font-publicSans lg:text-[32px] text-2xl font-semibold text-white text-center'>Subscribe to our newsletter</h3>
+        <p className='font-publicSans text-sm text-gray-300 text-center py-5 lg:px-15'>Praesent fringilla erat a lacinia egestas. Donec vehicula tempor libero et cursus. Donec non quam urna. Quisque vitae porta ipsum.</p>
         <div className="bg-white py-3 px-3 flex gap-4">
-            <input className='bg-white w-105 border-none outline-none focus:ring-0 py-3 pl-5' type="text" placeholder='Email address'/>
-            <Link href="#" className="bg-[#FF6E14] hover:bg-[rgba(206,97,30,0.97)] uppercase text-white hover:text-gray-300 py-3 px-8 no-underline flex items-center gap-2">Subscribe <FaArrowRight /></Link>
+            <input className='bg-white w-full border-none outline-none focus:ring-0 py-3 pl-5' type="text" placeholder='Email address'/>
+            <Link href="#" className="bg-[#FF6E14] hover:bg-[rgba(206,97,30,0.97)] uppercase text-white hover:text-gray-300 lg:py-3  lg:text-[16px] text-[12px] lg:px-8 px-4 no-underline flex items-center gap-2">Subscribe <FaArrowRight /></Link>
         </div>
         <div className="flex justify-between px-5 py-3">
-            <Image src="/NewsOne.svg" alt="newsletter" width={80} height={80}/>
-            <Image src="/NewsTwo.svg" alt="newsletter" width={80} height={80}/>
-            <Image src="/NewsThree.svg" alt="newsletter" width={80} height={80}/>
-            <Image src="/NewsFour.svg" alt="newsletter" width={80} height={80}/>
-            <Image src="/NewsFive.svg" alt="newsletter" width={80} height={80}/>
+            <Image className='w-1/5 p-4' src="/NewsOne.svg" alt="newsletter" width={80} height={80}/>
+            <Image className='w-1/5 p-4' src="/NewsTwo.svg" alt="newsletter" width={80} height={80}/>
+            <Image className='w-1/5 p-4' src="/NewsThree.svg" alt="newsletter" width={80} height={80}/>
+            <Image className='w-1/5 p-4' src="/NewsFour.svg" alt="newsletter" width={80} height={80}/>
+            <Image className='w-1/5 p-4' src="/NewsFive.svg" alt="newsletter" width={80} height={80}/>
         </div>
       </Container>
     </div>
