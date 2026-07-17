8e7ee2a3473ecb88b3a9aaabe7dff493ba3847a8
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-17 14:18:25 +0600

    cart responsive

--- app/(root)/cart/page.jsx
+++ app/(root)/cart/page.jsx
@@ -28,7 +28,7 @@ const page = () => {
     <div>
     <Container>
       <div className="pt-10">
-        <h2 className='text-4xl font-bold' >Cart</h2>
+        <h2 className='lg:text-4xl text-2xl font-bold' >Cart</h2>
       </div>
       <div className="py-20">
         <div className="">
@@ -37,23 +37,23 @@ const page = () => {
         <div className="">
           <div className="flex  bg-[#1b6392] text-white">
         <div className="w-2/5 py-5 border border-b-0 border-r-0 border-[#dbdbdb] text-center">
-        <h3 className='font-bold text-xl'>Product Details</h3>
+        <h3 className='font-bold lg:text-xl text-[14px]'>Product Details</h3>
         </div>
         <div className="w-1/5 py-5 border border-b-0 border-r-0 border-[#dbdbdb] text-center">
-        <h3 className='font-bold text-xl'>Price</h3>
+        <h3 className='font-bold lg:text-xl text-[14px]'>Price</h3>
         </div>
         <div className="w-1/5 py-5 border border-b-0 border-r-0 border-[#dbdbdb] text-center">
-        <h3 className='font-bold text-xl'>Quantity</h3>
+        <h3 className='font-bold lg:text-xl text-[14px]'>Quantity</h3>
         </div>
         <div className="w-1/5 py-5 border border-b-0  border-[#dbdbdb] text-center">
-        <h3 className='font-bold text-xl'>Total</h3>
+        <h3 className='font-bold lg:text-xl text-[14px]'>Total</h3>
         </div>
       </div>
       <div className="">
         {cartData.map((item) => (
         <div key={item.id} className="">
            <div className="flex ">
-          <div className="w-2/5 py-3 border border-t-0 border-r-0 border-[#dbdbdb]  flex justify-center items-center gap-5">
+          <div className="w-2/5 py-3 border border-t-0 border-r-0 border-[#dbdbdb]  flex justify-center items-center lg:gap-5">
           <div onClick={()=>handleRemove(item)} className="border border-[#dbdbdb] h-5 w-5 cursor-pointer flex items-center justify-center">
             <RxCross2 />
           </div>
@@ -61,20 +61,20 @@ const page = () => {
               <div className="h-15 w-15 border border-[#f5f5f5]">
                 <img src={item.thumbnail} alt={item.id} />
               </div>
-              <h3 className='font-bold text-xl'>{item.title}</h3>
+              <h3 className='font-bold lg:text-xl text-[14px]'>{item.title}</h3>
             </div>
           </div>
-          <div className="w-1/5 py-3 border border-t-0 border-r-0 border-[#dbdbdb] flex items-center justify-center">
+          <div className="w-1/5 py-3 border border-t-0 border-r-0 border-[#dbdbdb] flex items-center justify-center lg:text-[16px] text-[12px]">
             $ {((item.price - (item.price * item.discountPercentage) / 100)).toFixed(2)}
           </div>
           <div className="w-1/5 py-3 flex justify-center items-center gap-2 border border-t-0 border-r-0 border-[#dbdbdb]">
           <div onClick={()=> handleDecrement(item)} className="border border-[#dbdbdb] cursor-pointer p"><FiMinus/></div>
-          <div className="">
+          <div className="lg:text-[16px] text-[12px]">
             {item.qun}
           </div>
           <div onClick={()=>handleIncrement(item)} className="border border-[#dbdbdb] cursor-pointer p"><FiPlus/></div>
           </div>
-          <div className="w-1/5 py-3 flex justify-center items-center border border-t-0  border-[#dbdbdb]">
+          <div className="w-1/5 py-3 flex justify-center items-center border border-t-0  border-[#dbdbdb] lg:text-[16px] text-[12px]">
           <h3>$ {(((item.price - (item.price * item.discountPercentage) / 100))* item.qun).toFixed(2)}</h3>
           </div>
         </div>
@@ -106,7 +106,7 @@ const page = () => {
           <div className="">
             {cartData.length > 0 &&(
               <div className="flex justify-center">
-                <Link className='bg-[#1b6392] py-3 px-8 text-white cursor-pointer rounded-xl' href="/checkout">checkout</Link>
+                <Link className='bg-[#1b6392] py-3 px-8 text-white cursor-pointer rounded-xl lg:text-[16px] text-[12px]' href="/checkout">checkout</Link>
               </div>
             )}
           </div>
